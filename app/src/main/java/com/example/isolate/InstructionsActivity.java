428f10612814c5b0339195b555bda267d7d2ec93
Chris <cifurin@hotmail.com>
2018-12-19 11:22:09 +0000

    updated activity support

--- app/src/main/java/com/example/isolate/InstructionsActivity.java
+++ app/src/main/java/com/example/isolate/InstructionsActivity.java
@@ -2,6 +2,7 @@ package com.example.isolate;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.webkit.WebView;
 
 public class InstructionsActivity extends Activity {
 
@@ -9,6 +10,14 @@ public class InstructionsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
+
+        // get our html content
+        String htmlAsString = getString(R.string.test);
+
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.loadDataWithBaseURL(null, htmlAsString, "text/html", "utf-8", null);
+
     }
 
 }
